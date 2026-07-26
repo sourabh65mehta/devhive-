3570cce8361eeaa697502acfed5b4ff6e85587e8
sourabh65mehta <sourabh65mehta@gmail.com>
2026-07-26 17:18:43 +0530

    fix: add fallback timespan strings for jwt expiresIn option

--- Backend/src/utils/generateTokens.js
+++ Backend/src/utils/generateTokens.js
@@ -1,19 +1,21 @@
 import jwt from 'jsonwebtoken';
 
 const generateAccessTokens = (user) => {
+   const expiresIn = process.env.ACCESS_TOKEN_EXPIRES_IN || "15m";
    return jwt.sign(
         { id: user.id, username: user.username, email: user.email },
         process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN }
+        { expiresIn }
     );
 };
 
 const generateRefreshTokens = (user) => {
+    const expiresIn = process.env.REFRESH_TOKEN_EXPIRES_IN || "7d";
     return jwt.sign(
         { id: user.id },
         process.env.REFRESH_TOKEN_SECRET,
-        { expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN }
+        { expiresIn }
     );
 };
 
-export { generateAccessTokens, generateRefreshTokens }; 
+export { generateAccessTokens, generateRefreshTokens };
